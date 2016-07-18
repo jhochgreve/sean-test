ecfc683486e7958d27a5f5efec4452e1daac51de
Joe Hochgreve <jhochgreve@trafficdigitalagency.com>
2016-07-18 11:22:19 -0400

    Updated blog header styles

---
+++ cache/compiled/files/1bee57b0e4c927b4f09bfe25c63f9bcd.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Applications/MAMP/htdocs/sean/user/accounts/joe.yaml',
+    'modified' => 1468376236,
+    'data' => [
+        'email' => 'jhochgreve@gmail.com',
+        'fullname' => 'Joe Hochgreve',
+        'title' => 'Developer',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$7b7r83SAxDrP.cccP0OYde1I2eJTTQ.s0rxoBFZH0/xj2HOe2JkDS'
+    ]
+];
